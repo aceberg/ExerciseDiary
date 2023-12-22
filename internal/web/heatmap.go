86591d8b0e75c32e9e9dcec9364e3ec5f78cf33f
aceberg <1502200+aceberg@users.noreply.github.com>
2023-12-22 22:13:10 +0700

    ApexCharts HeatMap

---
+++ internal/web/heatmap.go
@@ -0,0 +1,63 @@
+package web
+
+import (
+	"log"
+	"time"
+
+	"github.com/aceberg/ExerciseDiary/internal/models"
+)
+
+func generateHeatMap() (heatMap []models.HeatMapData) {
+	var heat models.HeatMapData
+	var data models.HeatPlace
+
+	w := 52 // weeks to show
+
+	max := time.Now()
+	min := max.AddDate(0, 0, -7*w)
+
+	startDate := weekStartDate(min)
+	countMap := countHeat()
+
+	dow := []string{"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"}
+
+	for _, day := range dow {
+
+		heat.Name = day
+		heat.Data = []models.HeatPlace{}
+
+		for i := 0; i < w+1; i++ {
+
+			data.X = startDate.AddDate(0, 0, 7*i).Format("2006-01-02")
+			data.Y = countMap[data.X]
+			heat.Data = append(heat.Data, data)
+		}
+
+		heatMap = append(heatMap, heat)
+		startDate = startDate.AddDate(0, 0, 1)
+	}
+
+	return heatMap
+}
+
+func weekStartDate(date time.Time) time.Time {
+	offset := (int(time.Monday) - int(date.Weekday()) - 7) % 7
+	result := date.Add(time.Duration(offset*24) * time.Hour)
+	return result
+}
+
+func countHeat() map[string]int {
+	countMap := make(map[string]int)
+
+	for _, ex := range exData.Sets {
+		reps, exists := countMap[ex.Date]
+		if exists {
+			countMap[ex.Date] = reps + 1
+		} else {
+			countMap[ex.Date] = 1
+		}
+	}
+	log.Println("COUNT", countMap)
+
+	return countMap
+}
--- internal/web/stats.go
+++ internal/web/stats.go
@@ -7,55 +7,18 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	// "github.com/aceberg/ExerciseDiary/internal/check"
+	"github.com/aceberg/ExerciseDiary/internal/db"
 	"github.com/aceberg/ExerciseDiary/internal/models"
 )
 
 func statsHandler(c *gin.Context) {
 	var guiData models.GuiData
 
-	guiData.Config = appConfig
+	exData.Sets = db.SelectSet(appConfig.DBPath)
 
+	guiData.Config = appConfig
 	guiData.HeatMap = generateHeatMap()
 
 	c.HTML(http.StatusOK, "header.html", guiData)
 	c.HTML(http.StatusOK, "stats.html", guiData)
-}
-
-func generateHeatMap() (heatMap []models.HeatMapData) {
-	var heat models.HeatMapData
-	var data models.HeatPlace
-
-	heat.Name = "Su"
-	data.X = "2024-12-24"
-	data.Y = 0
-	heat.Data = append(heat.Data, data)
-	data.X = "2024-12-17"
-	data.Y = 3
-	heat.Data = append(heat.Data, data)
-	heatMap = append(heatMap, heat)
-
-	heat.Data = []models.HeatPlace{}
-
-	heat.Name = "Sa"
-	data.X = "2024-12-23"
-	data.Y = 1
-	heat.Data = append(heat.Data, data)
-	data.X = "2024-12-16"
-	data.Y = 8
-	heat.Data = append(heat.Data, data)
-	heatMap = append(heatMap, heat)
-
-	heat.Data = []models.HeatPlace{}
-
-	heat.Name = "Fr"
-	data.X = "2024-12-22"
-	data.Y = 5
-	heat.Data = append(heat.Data, data)
-	data.X = "2024-12-15"
-	data.Y = 2
-	heat.Data = append(heat.Data, data)
-	heatMap = append(heatMap, heat)
-
-	return heatMap
 }
